b918fd71f272a8f1ad7adc94aa95b280171c65f3
That_Guy <eghove@gmail.com>
2019-10-02 15:07:37 -0500

    adding in basic nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+
+
+
+// pages
 import AboutPage from './pages/about/AboutPage';
 import HomePage from './pages/home/HomePage';
 import DetailsPage from './pages/details/DetailsPage';
 import Bad404Page from './pages/bad_404/Bad404Page';
+
+// shared components
+import NavBar from './shared_components/NavBar/NavBar';
+
 // import api from './utils/api.js';
+
+// styles
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
 
 // api.test();
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/details/:parkcode" component={DetailsPage} />
-          <Route default component={Bad404Page} />
+      <Container>
+        <NavBar></NavBar>
+     
+        <Router>
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route exact path="/about" component={AboutPage} />
+            <Route exact path="/details/:parkcode" component={DetailsPage} />
+            <Route default component={Bad404Page} />
+          </Switch>
+        </Router>
 
-
-        </Switch>
-      </Router>
+      </Container>
 
     </div>
   );
