fcd1eb9a46b6c8ab8bf9a38ccf4b4b158879b105
That_Guy <eghove@gmail.com>
2019-10-06 16:50:14 -0500

    starting to style details page

--- client/src/pages/details/DetailsPage.js
+++ client/src/pages/details/DetailsPage.js
@@ -4,12 +4,13 @@ import Col from 'react-bootstrap/Col';
 // custom components
 import BasicParkInfo from './components/BasicParkInfo/BasicParkInfo';
 import GeneralWeatherInfo from './components/GeneralWeatherInfo/GeneralWeatherInfo';
-import DetailedWeatherForecast, {WeatherForecastItem} from './components/DetailedWeatherForecast/DetailedWeatherForecast'
-import CampgroundsInfo, {CampgroundItem} from './components/CampgroundsInfo/CampgroundsInfo';
+import DetailedWeatherForecast, { WeatherForecastItem } from './components/DetailedWeatherForecast/DetailedWeatherForecast'
+import CampgroundsInfo, { CampgroundItem } from './components/CampgroundsInfo/CampgroundsInfo';
 
 // utilities
 import api from '../../utils/apiParkDetail';
-
+// styling
+import './detailsPage.css';
 
 
 class DetailsPage extends React.Component {
@@ -17,7 +18,8 @@ class DetailsPage extends React.Component {
   state = {
     npsBasicData: [],
     weatherForecast: [],
-    campgrounds: []
+    campgrounds: [],
+    campgroundsExist: null
   }
 
   code = this.props.match.params.parkcode;
@@ -35,7 +37,7 @@ class DetailsPage extends React.Component {
     parkcode = parkcode.toString();
     api.weatherInfo(parkcode)
       .then(results => {
-        this.setState({ weatherForecast: results.data.properties.periods})
+        this.setState({ weatherForecast: results.data.properties.periods })
       })
       .catch(error => console.log(error))
   }
@@ -45,13 +47,18 @@ class DetailsPage extends React.Component {
     api.campgroundInfo(parkcode)
       .then(results => {
         if (results.data.data.length > 0) {
-          this.setState({ campgrounds: results.data.data})
+          this.setState(
+            {
+              campgrounds: results.data.data,
+              campgroundsExist: true
+            }
+          )
         }
         // some parks don't have campground, need to figure out how to account for that.
         else {
-          this.setState({campgrounds: "No campgrounds found"})
+          this.setState({ campgroundsExist: false })
         }
-       
+
         // console.log(results.data.data);
       })
       .catch(error => console.log(error))
@@ -65,27 +72,26 @@ class DetailsPage extends React.Component {
 
   render() {
     return (
-      <Row>
-        {this.state.npsBasicData.length === 0 || this.state.weatherForecast.length === 0 
-        || this.state.campgrounds.length === 0
-         ?
+      <Row className="detailsPage-container">
+        {this.state.npsBasicData.length === 0 || this.state.weatherForecast.length === 0 || this.state.campgroundsExist === null
+          ?
           <Col md={12}>
             <h2>Loading...</h2>
           </Col>
           :
           <Col md={12}>
             <BasicParkInfo
-             heading={this.state.npsBasicData.data[0].fullName}
-             description={this.state.npsBasicData.data[0].description}
+              heading={this.state.npsBasicData.data[0].fullName}
+              description={this.state.npsBasicData.data[0].description}
             />
             <GeneralWeatherInfo
               weatherInfo={this.state.npsBasicData.data[0].weatherInfo}
             />
             <DetailedWeatherForecast>
-              {this.state.weatherForecast.map(weather=>{
-                return(
-                  <WeatherForecastItem 
-                    name = {weather.name}
+              {this.state.weatherForecast.map(weather => {
+                return (
+                  <WeatherForecastItem
+                    name={weather.name}
                     temperature={weather.temperature}
                     forecast={weather.detailedForecast}
                   />
@@ -94,19 +100,24 @@ class DetailsPage extends React.Component {
 
             </DetailedWeatherForecast>
 
-            <CampgroundsInfo>
-              {this.state.campgrounds.map(camp=>{
-                return(
-                  <CampgroundItem 
-                    name={camp.name}
-                    description={camp.description}
-                  
-                  />
-                )
-              })}
-            </CampgroundsInfo>
+            {this.state.campgroundsExist === true ?
+              <CampgroundsInfo>
+                {this.state.campgrounds.map(camp => {
+                  return (
+                    <CampgroundItem
+                      name={camp.name}
+                      description={camp.description}
 
-            {console.log(this.state.campgrounds)}
+                    />
+                  )
+                })}
+              </CampgroundsInfo>
+              : 
+              <p>No campgrounds found for this park!</p>
+            }
+
+
+
 
 
           </Col>}
--- client/src/pages/details/components/GeneralWeatherInfo/GeneralWeatherInfo.js
+++ client/src/pages/details/components/GeneralWeatherInfo/GeneralWeatherInfo.js
@@ -2,14 +2,25 @@ import React from 'react';
 // bootstrap components
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 
 function GeneralWeatherInfo(props) {
   return (
-    <Row>
-      <Col md={12}>
-        <h3>General Weather Information</h3>
-        <p>{props.weatherInfo}</p>
+    <Row className="generalInfoRow">
+      <Col md={6}>
+        <Card>
+          <Card.Body>
+            <Card.Title>
+              <h3>General Weather Information</h3>
+            </Card.Title>
+            <Card.Text>
+              <p>{props.weatherInfo}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+
       </Col>
     </Row>
   )
