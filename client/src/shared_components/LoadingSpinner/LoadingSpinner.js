48c977083f3b6a553084023c8733f98343b7c4be
That_Guy <eghove@gmail.com>
2019-10-07 10:16:03 -0500

    spinner accessibility tweak

--- client/src/shared_components/LoadingSpinner/LoadingSpinner.js
+++ client/src/shared_components/LoadingSpinner/LoadingSpinner.js
@@ -3,7 +3,9 @@ import Spinner from 'react-bootstrap/Spinner';
 
 function LoadingSpinner() {
   return (
-    <Spinner animation="border" variant="success" />
+    <Spinner animation="border" variant="success" role="status">
+      <span className="sr-only">Loading...</span>
+    </Spinner>
   )
 }
 
