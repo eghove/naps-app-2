2c06f7c3bddd8fe04ade700e4b1b028516b1017c
That_Guy <eghove@gmail.com>
2019-10-12 08:30:25 -0500

    moving functions for the homepage down from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,6 @@ import Bad404Page from './pages/bad_404/Bad404Page';
 // shared components
 import NavBar from './shared_components/NavBar/NavBar';
 
-// API utility
-import api from './utils/api.js';
-
 // styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -22,64 +19,18 @@ import './App.css';
 
 
 class App extends React.Component {
-  // ====SETTING STATE====
-  state = {
-    // keeps track of what the user types in the search bar
-    currentSearchTerm: '',
-    // keeps track of the search term once user presses search
-    searchedTerm: '',
-    // array that keeps the search results
-    searchResults: []
-  }
-
-  // ====HOME PAGE FUNCTIONS====
-
-  updateCurrentSearchTerm = (event) => {
-    let value = event.target.value;
-    // TO DO: Some sort of parser to make sure the search term can be used by API
-
-    // set the state to the current term
-    this.setState({ currentSearchTerm: value },
-      () => { console.log(this.state.currentSearchTerm); });
-  }
-
-  updateSearchedTerm = (event) => {
-    event.preventDefault();
-
-    // TO DO: More logic checks to make sure
-    if (this.state.currentSearchTerm !== '') {
-      this.setState({
-        searchedTerm: this.state.currentSearchTerm,
-        // clear out search results 
-        searchResults: []
-      },
-        () => {
-          // console.log(this.state.searchedTerm);
-          api.keywordSearch(this.state.searchedTerm)
-            .then(results => this.setState({ searchResults: results.data.data }, () => console.log(this.state.searchResults)))
-            // .then(results=> console.log(results.data.data))
-            .catch(error => console.log(error));
-        });
-    }
-    else return;
-  }
-
-
-
-
-
-
+ 
 
   render() {
     return (
       <div className="App">
         <Container>
           <NavBar></NavBar>
+          {console.log(this.state)}
 
           <Router>
             <Switch>
-              <Route exact path="/"
-                render={props => <HomePage updateCurrentSearch={this.updateCurrentSearchTerm} updateSearchedTerm={this.updateSearchedTerm} searchResults={this.state.searchResults} />} />
+              <Route exact path="/" component={HomePage}/>
               <Route exact path="/about" component={AboutPage} />
               <Route exact path="/details/:parkcode" component={DetailsPage} />
               <Route default component={Bad404Page} />
--- client/src/pages/home/HomePage.js
+++ client/src/pages/home/HomePage.js
@@ -6,44 +6,94 @@ import Col from 'react-bootstrap/Col';
 import JumbotronHome from './components/JumbotronHome/JumbotronHome';
 import SearchBar from './components/SearchBar/SearchBar';
 import ResultsDisplay, { ResultsDisplayItem } from './components/ResultsDisplay/ResultsDisplay';
+
+// API utility
+import api from '../../utils/api';
 // import local styling
 import './homepage.css';
 
 
 
-function HomePage(props) {
-  return (
-    <Row className="homePage-container">
-      <Col xs={12}>
-        <JumbotronHome
-          heading={"Welcome to NaPS App 2.0!"}
-          hero={"Use the search bar below to learn more about the National Parks of the United States."}
-        />
-        <SearchBar
-          placeholder={"Search for a National Park using keywords! (Try 'Grand')"}
-          updateCurrentSearch={props.updateCurrentSearch}
-          updateSearchedTerm={props.updateSearchedTerm}
-        
-        />
-        <ResultsDisplay>
-          {props.searchResults.map(parks=>{
-            return (
-              <ResultsDisplayItem
-                park_name={parks.fullName}
-                park_description={parks.description}
-                park_code = {parks.parkCode}
-              
-              
-              />
+class HomePage extends React.Component {
 
-              
-            )
-          })}
-        </ResultsDisplay>
+  // ====SETTING STATE====
+  state = {
+    // keeps track of what the user types in the search bar
+    currentSearchTerm: '',
+    // keeps track of the search term once user presses search
+    searchedTerm: '',
+    // array that keeps the search results
+    searchResults: []
+  }
 
-      </Col>
-    </Row>
-  )
+  // ====HOME PAGE FUNCTIONS====
+
+  updateCurrentSearchTerm = (event) => {
+    let value = event.target.value;
+    // TO DO: Some sort of parser to make sure the search term can be used by API
+
+    // set the state to the current term
+    this.setState({ currentSearchTerm: value },
+      () => { console.log(this.state.currentSearchTerm); });
+  }
+
+  updateSearchedTerm = (event) => {
+    event.preventDefault();
+
+    // TO DO: More logic checks to make sure
+    if (this.state.currentSearchTerm !== '') {
+      this.setState({
+        searchedTerm: this.state.currentSearchTerm,
+        // clear out search results 
+        searchResults: []
+      },
+        () => {
+          // console.log(this.state.searchedTerm);
+          api.keywordSearch(this.state.searchedTerm)
+            .then(results => this.setState({ searchResults: results.data.data }, () => console.log(this.state.searchResults)))
+            // .then(results=> console.log(results.data.data))
+            .catch(error => console.log(error));
+        });
+    }
+    else return;
+  }
+
+  render() {
+    return (
+      <Row className="homePage-container">
+        <Col xs={12}>
+          <JumbotronHome
+            heading={"Welcome to NaPS App 2.0!"}
+            hero={"Use the search bar below to learn more about the National Parks of the United States."}
+          />
+          <SearchBar
+            placeholder={"Search for a National Park using keywords! (Try 'Grand')"}
+            updateCurrentSearch={this.updateCurrentSearchTerm}
+            updateSearchedTerm={this.updateSearchedTerm}
+
+          />
+
+          <ResultsDisplay>
+            {this.state.searchResults.map(parks => {
+              return (
+                <ResultsDisplayItem
+                  park_name={parks.fullName}
+                  park_description={parks.description}
+                  park_code={parks.parkCode}
+
+
+                />
+
+
+              )
+            })}
+          </ResultsDisplay>
+
+        </Col>
+      </Row>
+    )
+  }
+
 }
 
 export default HomePage;
