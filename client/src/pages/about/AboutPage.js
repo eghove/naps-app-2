9f2dbf0563739191f236285af1762485e09e0962
That_Guy <eghove@gmail.com>
2019-10-12 12:44:39 -0500

    404 page and some additions to readme while I was in there

--- client/src/pages/about/AboutPage.js
+++ client/src/pages/about/AboutPage.js
@@ -11,23 +11,23 @@ function AboutPage(props) {
       <Row>
         <Col md={12}>
           <Jumbotron>
-            <h2>About NaPS App 2.1</h2>
+            <h2>About NaPS App 2.0</h2>
           </Jumbotron>
         </Col>
       </Row>
       <Row>
         <Col md={{ span: 10, offset: 1}} className={"text-left text-holder"}>
           <p>
-            NaPS App 2.1 is a project by Eric Hove. It is an extension of the original NaPS App built by Eric Hove, Alan Wagner, and Andrew Burns. <a rel="noopener noreferrer" target="_blank" href="https://github.com/eghove/NaPS-App">Click here to learn more about the original NaPS App.</a>
+            NaPS App 2.0 is a project by Eric Hove. It is an extension of the original NaPS App built by Eric Hove, Alan Wagner, and Andrew Burns. <a rel="noopener noreferrer" target="_blank" href="https://github.com/eghove/NaPS-App">Click here to learn more about the original NaPS App.</a>
           </p>
           <p>
-            NaPS App 2.1 aims to make the public data hosted by the National Parks Service (NPS) and the National Oceanic and Atmospheric Administration (NOAA) accessible to users planning a trip to a National Park in the United States.
+            NaPS App 2.0 aims to make the public data hosted by the National Parks Service (NPS) and the National Oceanic and Atmospheric Administration (NOAA) accessible to users planning a trip to a National Park in the United States.
           </p>
           <p>
-            Additional updates are coming. If you'd like to learn more about Eric Hove, <a rel="noopener noreferrer" target="_blank" href="https://eghove.github.io">click here.</a> If you'd like to view the source code for NaPS App 2.1, <a rel="noopener noreferrer" target="_blank" href="https://github.com/eghove/naps-app-2">click here.</a>
+            Additional updates are coming. If you'd like to learn more about Eric Hove, <a rel="noopener noreferrer" target="_blank" href="https://eghove.github.io">click here.</a> If you'd like to view the source code for NaPS App 2.0, <a rel="noopener noreferrer" target="_blank" href="https://github.com/eghove/naps-app-2">click here.</a>
           </p>
           <p>
-            NaPS App 2.1 last updated on {props.updated_date}.
+            NaPS App 2.0 last updated on {props.updated_date}.
           </p>
         </Col>
       </Row>
--- client/src/pages/bad_404/Bad404Page.js
+++ client/src/pages/bad_404/Bad404Page.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import './bad404Page.css';
+
 
 function Bad404Page() {
   return (
-    <div className="404-container">
-      <p>Page not found</p>
-    </div>
+    <Row className="bad404Page-container">
+      <Col md={{ span: 10, offset: 1 }} className={"text-left text-holder"}>
+        <h2>Page Not Found</h2>
+        <p>
+          Regrettably, this page was not found. Please <a href="/">click here to navigate back Home.</a>
+        </p>
+      </Col>
+    </Row>
   )
 }
 
